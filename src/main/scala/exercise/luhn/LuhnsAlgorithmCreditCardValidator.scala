c304f4976b26d9f4df527d5770b80a89a63b6230
Toby <toby.weston@db.com>
2018-10-10 21:23:10 +0100

    double every other digit

--- src/main/scala/exercise/luhn/LuhnsAlgorithmCreditCardValidator.scala
+++ src/main/scala/exercise/luhn/LuhnsAlgorithmCreditCardValidator.scala
@@ -8,6 +8,16 @@ class LuhnsAlgorithmCreditCardValidator extends CreditCardNumberValidator {
 }
 
 object LuhnsAlgorithmCreditCardValidator {
+  def doubleEverySecondDigitReverseOrder(number: Long): List[Int] = {
+    separate(number)
+      .reverse
+      .zipWithIndex
+      .map {
+        case (value, index) if index % 2 == 0 => value
+        case (value, _)                       => value * 2
+      }
+      .reverse
+  }
 
   def separate(number: Long): List[Int] = {
     number.toString.map(_.asDigit).toList
--- src/test/scala/exercise/luhn/LuhnsAlgorithmCreditCardValidatorTest.scala
+++ src/test/scala/exercise/luhn/LuhnsAlgorithmCreditCardValidatorTest.scala
@@ -1,11 +1,20 @@
 package exercise.luhn
 
-import exercise.luhn.LuhnsAlgorithmCreditCardValidator.separate
+import exercise.luhn.LuhnsAlgorithmCreditCardValidator.{doubleEverySecondDigitReverseOrder, separate}
 import org.hamcrest.CoreMatchers.is
 import org.junit.Assert.assertThat
 import org.junit.Test
 
 class LuhnsAlgorithmCreditCardValidatorTest {
+
+  @Test def doublesTheNumber(): Unit = {
+    assertThat(doubleEverySecondDigitReverseOrder(0L), is(List(0)))
+    assertThat(doubleEverySecondDigitReverseOrder(1L), is(List(1)))
+    assertThat(doubleEverySecondDigitReverseOrder(21L), is(List(4, 1)))
+    assertThat(doubleEverySecondDigitReverseOrder(121L), is(List(1, 4, 1)))
+    assertThat(doubleEverySecondDigitReverseOrder(3121L), is(List(6, 1, 4, 1)))
+    assertThat(doubleEverySecondDigitReverseOrder(1234567890L), is(List(2, 2, 6, 4, 10, 6, 14, 8, 18, 0)))
+  }
 
   @Test def separateANumber(): Unit = {
     assertThat(separate(1L), is(List(1)))
