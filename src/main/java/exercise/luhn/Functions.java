3fa926cefd7e33092388cea1600c0bc4cd89ac99
Toby <toby.weston@db.com>
2018-10-10 15:41:34 +0100

    basic doubling every second number

--- src/main/java/exercise/luhn/Functions.java
+++ src/main/java/exercise/luhn/Functions.java
@@ -1,13 +1,36 @@
 package exercise.luhn;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.IntUnaryOperator;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+import static java.util.Collections.*;
+import static java.util.stream.Collectors.*;
 
 public class Functions {
 
-    public static List<Integer> separate(Long number) {
-        return number.toString().chars()
-                .map(c -> Integer.parseInt(String.valueOf((char) c))).boxed()
-                .collect(Collectors.toList());
+    public static List<Integer> doubleEverySecondDigitReverseOrder(Long number) {
+        List<Integer> values = separate(number);
+        reverse(values);
+        List<Integer> doubled = IntStream
+                .range(0, values.size())
+                .map(index -> index % 2 == 0 ? values.get(index) : values.get(index) * 2)
+                .boxed()
+                .collect(toList());
+        reverse(doubled);
+        return doubled;
     }
+
+    public static List<Integer> separate(Long number) {
+        return number.toString()
+                .chars()
+                .map(c -> Integer.parseInt(String.valueOf((char) c)))
+                .boxed()
+                .collect(toList());
+    }
+
 }
--- src/test/java/exercise/luhn/FunctionsTest.java
+++ src/test/java/exercise/luhn/FunctionsTest.java
@@ -2,6 +2,7 @@ package exercise.luhn;
 
 import org.junit.Test;
 
+import static exercise.luhn.Functions.doubleEverySecondDigitReverseOrder;
 import static java.util.Arrays.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
@@ -9,7 +10,17 @@ import static org.junit.Assert.*;
 public class FunctionsTest {
 
     @Test
-    public void seperatesANumber() {
+    public void doublesTheNumber() {
+        assertThat(doubleEverySecondDigitReverseOrder(0L), is(asList(0)));
+        assertThat(doubleEverySecondDigitReverseOrder(1L), is(asList(1)));
+        assertThat(doubleEverySecondDigitReverseOrder(21L), is(asList(4, 1)));
+        assertThat(doubleEverySecondDigitReverseOrder(121L), is(asList(1, 4, 1)));
+        assertThat(doubleEverySecondDigitReverseOrder(3121L), is(asList(6, 1, 4, 1)));
+        assertThat(doubleEverySecondDigitReverseOrder(1234567890L), is(asList(2, 2, 6, 4, 10, 6, 14, 8, 18, 0)));
+    }
+
+    @Test
+    public void separateANumber() {
         assertThat(Functions.separate(1L), is(asList(1)));
         assertThat(Functions.separate(21L), is(asList(2, 1)));
         assertThat(Functions.separate(321L), is(asList(3, 2, 1)));
